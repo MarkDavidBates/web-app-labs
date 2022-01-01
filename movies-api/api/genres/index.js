b8d601dec08fee4aee5cdf51e4bbf10f34386f4a
Mark Bates <markbates.339@gmail.com>
2022-01-01 18:33:02 +0000

    adjusted code to work with movie app

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import Genre from './genreModel';
 import asyncHandler from 'express-async-handler';
+import { getGenres } from '../tmdb-api';
 
 const router = express.Router(); 
 
 // Get all genres
 router.get('/', async (req, res) => {
-    const genres = await Genre.find();
+    const genres = await getGenres();
     res.status(200).json(genres);
 });
 
--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -36,8 +36,8 @@ export async function loadMovies() {
   console.log('load seed data');
   console.log(movies.length);
   try {
-    await movieModel.deleteMany();
-    await movieModel.collection.insertMany(movies);
+    //await movieModel.deleteMany();
+    //await movieModel.collection.insertMany(movies);
     console.info(`${movies.length} Movies were successfully stored.`);
   } catch (err) {
     console.error(`failed to Load movie Data: ${err}`);
